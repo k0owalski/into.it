6c649c827be114f18c25ad024369e15073a5c111
kowalski <knrd.kowalik@gmail.com>
2023-05-04 17:33:48 +0200

    Api routes prepared

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import routes from 'routes/routes';
+
 const app = express();
 
 dotenv.config();
+
+app.use('/api', routes);
 
 app.listen(process.env.PORT, () => {
 	console.log('listening on port ' + process.env.PORT);
